6e7d2f58e973f7dfabbb6741196ea02a5e2b541d
Carlos <carlos.asrc@gmail.com>
2026-08-20 10:23:52 -0300

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        seen = {}
+
+        for s in strs:
+            frequency = [0] * 26
+            for l in s:
+                frequency[ord(l) - ord('a')] += 1
+            key = tuple(frequency)
+            if key in seen:
+                seen[key].append(s)
+            else:
+                seen[key] = [s]
+        return list(seen.values())
+
+        
+        
